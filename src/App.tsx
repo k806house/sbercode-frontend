613bc9f3c62a64b5dc986483d91ef8686d5da7bb
kisasexypantera94 <green.grinya@gmail.com>
2021-02-28 00:50:21 +0300

    add choose_cafe/add_item handlers

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React, { FC, memo, useReducer, useState, useRef, useEffect } from 'react';
 import { createSmartappDebugger, createAssistant, AssistantAppState } from '@sberdevices/assistant-client';
 import './App.css';
-import { Menu } from './Menu';
+
+import { reducer } from './store';
 
 const initializeAssistant = (getState: any) => {
     if (process.env.NODE_ENV === 'development' && window.Cypress == null) {
@@ -16,11 +17,16 @@ const initializeAssistant = (getState: any) => {
 };
 
 export const App: FC = memo(() => {
-    // const [appState, dispatch] = useReducer(reducer, {
-    //     notes: [{ id: 'uinmh', title: 'купить хлеб', completed: false }],
-    // });
+    const [appState, dispatch] = useReducer(reducer, {
+        notes: [{ id: 'uinmh', title: 'купить хлеб', completed: false }],
+        user_stages: new Map(),
+        user_carts: new Map(),
+        user_cafes: new Map(),
+        id_to_item: new Map(),
+    });
 
-    const [note, setNote] = useState('');
+    const [note, setNote] = useState('kekis');
+    console.log(note);
 
     const assistantStateRef = useRef<AssistantAppState>();
     const assistantRef = useRef<ReturnType<typeof createAssistant>>();
@@ -29,6 +35,7 @@ export const App: FC = memo(() => {
         assistantRef.current = initializeAssistant(() => assistantStateRef.current);
 
         assistantRef.current.on('data', ({ navigation, action }: any) => {
+            console.log(action);
             if (navigation) {
                 switch (navigation.command) {
                     case 'UP':
@@ -40,26 +47,72 @@ export const App: FC = memo(() => {
                 }
             }
 
-            // if (action) {
-            //     dispatch(action);
-            // }
+            if (action) {
+                console.log("HAHA");
+                dispatch(action);
+            }
         });
     }, []);
 
-    // useEffect(() => {
-    //     assistantStateRef.current = {
-    //         item_selector: {
-    //             items: appState.notes.map(({ id, title }, index) => ({
-    //                 number: index + 1,
-    //                 id,
-    //                 title,
-    //             })),
-    //         },
-    //     };
-    // }, [appState]);
+    useEffect(() => {
+        assistantStateRef.current = {
+            item_selector: {
+                items: appState.notes.map(({ id, title }, index) => ({
+                    number: index + 1,
+                    id,
+                    title,
+                })),
+            },
+        };
+    }, [appState]);
 
+    const doneNote = (title: string) => {
+        assistantRef.current?.sendData({ action: { action_id: 'done', parameters: { title } } });
+    };
 
     return (
-        <Menu></Menu>
+        <main className="container">
+            <form
+                onSubmit={(event) => {
+                    event.preventDefault();
+                    dispatch({ type: 'add_note', note });
+                    setNote('');
+                }}
+            >
+                <input
+                    className="add-note"
+                    type="text"
+                    placeholder="Add Note"
+                    value={note}
+                    onChange={({ target: { value } }) => setNote(value)}
+                    required
+                    autoFocus
+                />
+            </form>
+            <ul className="notes">
+                {appState.notes.map((note, index) => (
+                    <li className="note" key={note.id}>
+                        <span>
+                            <span style={{ fontWeight: 'bold' }}>{index + 1}. </span>
+                            <span
+                                style={{
+                                    textDecorationLine: note.completed ? 'line-through' : 'none',
+                                }}
+                            >
+                                {note.title}
+                            </span>
+                        </span>
+                        <input
+                            id={`checkbox-note-${note.id}`}
+                            className="done-note"
+                            type="checkbox"
+                            checked={note.completed}
+                            onChange={() => doneNote(note.title)}
+                            disabled={note.completed}
+                        />
+                    </li>
+                ))}
+            </ul>
+        </main>
     );
 });
