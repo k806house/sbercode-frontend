d75840cf26f0440c590efb052c2b299882bd2ab2
I520950 <lizabezlutskaya@gmail.com>
2021-02-28 13:40:14 +0300

    add time picker

--- src/Alarm.tsx
+++ src/Alarm.tsx
@@ -4,12 +4,14 @@ import {Button} from "@sberdevices/ui/components/Button/Button";
 import {
     Card, CardContent, Cell, CellIcon, CellListItem,
     TextBox, TextBoxBigTitle,
-    MarkedList, MarkedItem} from '@sberdevices/ui';
+    MarkedList, MarkedItem, TimePicker
+} from '@sberdevices/ui';
 import {IconInfo, IconSpinner} from "@sberdevices/plasma-icons";
 import {primary} from "@sberdevices/plasma-tokens";
 import {Input} from "@sberdevices/ui/components/Input";
 import {Item} from "./store"
 import { UserStage } from './store';
+import {isSberBox} from "@sberdevices/ui/utils";
 
 type AlarmProps = {
     items: Item[];
@@ -23,6 +25,12 @@ export const Alarm: FC<AlarmProps> = memo((props: AlarmProps) => {
     items.forEach((item) => {
         total += item.price;
     });
+    const now = new Date();
+    const isSberbox = isSberBox();
+
+    function onChange() {
+        console.log("timepicker");
+    }
     return (
         <Container>
             <Card style={{ width: '20rem' }}>
@@ -60,41 +68,77 @@ export const Alarm: FC<AlarmProps> = memo((props: AlarmProps) => {
                             <TextBox> {total}</TextBox>
                         }
                     />
-
-                    {/*Address selections*/}
+                </CardContent>
+            </Card>
+            {/*Address selections*/}
+            <Card style={{ width: '20rem' }}>
+                <CardContent compact>
                     <Cell
-                        content={<TextBoxBigTitle>{'Доставка'}</TextBoxBigTitle>}
+                        content={<TextBoxBigTitle>{'Адрес'}</TextBoxBigTitle>}
                     />
                     <Row>
-                        <Col size={2} style={{ marginBottom: '1rem' }}>
+                        <Col size={6} style={{ marginBottom: '1rem' }}>
                             <Input placeholder={'Кв./офис'}></Input>
                         </Col>
-                        <Col size={2}>
+                        <Col size={6}>
                             <Input placeholder={'Подъезд'}></Input>
                         </Col>
                     </Row>
                     <Row>
-                        <Col size={2} style={{ marginBottom: '1rem' }}>
+                        <Col size={6} style={{ marginBottom: '1rem' }}>
                             <Input placeholder={'Домофон'}></Input>
                         </Col>
-                        <Col size={2}>
+                        <Col size={6}>
                             <Input placeholder={'Этаж'}></Input>
                         </Col>
                     </Row>
                     <Row>
-                        <Col size={2} style={{ marginBottom: '1rem' }}>
+                        <Col size={12} style={{ marginBottom: '1rem' }}>
                             <Input placeholder={'Комментарий'}></Input>
                         </Col>
-                        <Col size={2}>
+                    </Row>
+                    <Row>
+                        <Col size={12} style={{ marginBottom: '1rem' }}>
                             <Input placeholder={'Телефон'}></Input>
                         </Col>
                     </Row>
-                    <Input placeholder={'Время заказа'}></Input>
+                </CardContent>
+            </Card>
+            {/*Time selection*/}
+            <Card style={{ width: '20rem' }}>
+                <CardContent compact>
+                    <Cell
+                        content={<TextBoxBigTitle>{'Время'}</TextBoxBigTitle>}
+                    />
+                    <TimePicker
+                        value={
+                            new Date(
+                                now.getFullYear(),
+                                now.getMonth(),
+                                now.getDate(),
+                                0,
+                                30,
+                                56,
+                            )
+                        }
+                        min={new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 15, 29)}
+                        max={new Date(now.getFullYear(), now.getMonth(), now.getDate(), 12, 45, 59)}
+                        options={{
+                            hours: true,
+                            minutes: true,
+                            seconds: false,
+                        }}
+                        disabled={false}
+                        controls={isSberbox}
+                        onChange={onChange}
+                    />
                     <Button text="Оформить заказ"
                             view="primary"
                             onClick={() => props.setStage(UserStage.Final)}/>
                 </CardContent>
+
             </Card>
+
         </Container>
     );
 });
